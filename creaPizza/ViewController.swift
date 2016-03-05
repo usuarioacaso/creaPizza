498364f5d079005808ccc08dc85d82b8cf7d1271
Adrian <acaso@MacBook-Air-de-Adrian.local>
2016-03-05 11:52:38 -0500

    Tarea Coursera 2da semana

--- creaPizza/ViewController.swift
+++ creaPizza/ViewController.swift
@@ -9,17 +9,150 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tamano = ""
+    var masa = ""
+    var queso = ""
+    var ingrediente = ""
+    
+    var limpiaString:String = "Su Pizza"
 
+    @IBOutlet weak var resultadoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultadoLabel.text = limpiaString
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func chicaTamano(sender: UIButton) {
+        tamano = "Chica"
+        displayPizza()
     }
+    
+    @IBAction func mediaTamano(sender: UIButton) {
+        tamano = "Mediana"
+        displayPizza()
+    }
+    
+    @IBAction func granTamano(sender: UIButton) {
+        tamano = "Grande"
+        displayPizza()
+    }
+    
+    @IBAction func delgaMasa(sender: UIButton) {
+        masa = "Delgada"
+        displayPizza()
+    }
+    
+    @IBAction func crujiMasa(sender: UIButton) {
+        masa = "Crujiente"
+        displayPizza()
+    }
+    
+    @IBAction func gruesaMasa(sender: UIButton) {
+        masa = "Gruesa"
+        displayPizza()
+    }
+    
+    @IBAction func mozzaQueso(sender: UIButton) {
+        queso = "Mozzarella"
+        displayPizza()
+    }
+    
+    @IBAction func cheddQueso(sender: UIButton) {
+        queso = "Cheddar"
+        displayPizza()
+    }
+    
+    @IBAction func parmeQueso(sender: UIButton) {
+        queso = "Parmesano"
+        displayPizza()
+    }
+    
+    @IBAction func sinQueso(sender: UIButton) {
+        queso = "Sin queso"
+        displayPizza()
+    }
+    
+    @IBAction func jamIngred(sender: UIButton) {
+        ingrediente = "Jamon"
+        displayPizza()
+    }
+    
+    @IBAction func peppIngred(sender: UIButton) {
+        ingrediente = "Pepperoni"
+        displayPizza()
+    }
+    
+    @IBAction func pavoIngred(sender: UIButton) {
+        ingrediente = "Pavo"
+        displayPizza()
+    }
+    
+    @IBAction func salIngred(sender: UIButton) {
+        ingrediente = "Salchicha"
+        displayPizza()
+    }
+    
+    @IBAction func aceIngred(sender: UIButton) {
+        ingrediente = "Aceituna"
+        displayPizza()
+    }
+    
+    @IBAction func limpiaBoton(sender: UIButton) {
 
-
+        resultadoLabel.text = limpiaString
+        limpieza()
+    }
+    
+    func displayPizza(){
+        let muestraString = String(format: "%@ %@ %@ %@", tamano, masa, queso, ingrediente)
+        resultadoLabel.text = muestraString
+        
+    }
+    
+    func limpieza(){
+        tamano = ""
+        masa = ""
+        queso = ""
+        ingrediente = ""
+    }
+    
+    @IBAction func confirmaBoton(sender: UIButton) {
+        let confirma = ""
+        
+        switch confirma {
+            case tamano :
+            print("no hay tamano")
+            let alerta = UIAlertController(title: "Atencion!", message: "Falta el tamaño de la pizza", preferredStyle: .Alert)
+            let cancela = UIAlertAction(title: "Reintentar", style: .Cancel, handler: nil)
+            alerta.addAction(cancela)
+            self.presentViewController(alerta, animated:true, completion:nil)
+            case masa:
+            print("no hay masa")
+            let alerta = UIAlertController(title: "Atencion!", message: "Falta el tipo de Masa", preferredStyle: .Alert)
+            let cancela = UIAlertAction(title: "Reintentar", style: .Cancel, handler: nil)
+            alerta.addAction(cancela)
+            self.presentViewController(alerta, animated:true, completion:nil)
+            case queso:
+            print("no hay queso")
+            let alerta = UIAlertController(title: "Atencion!", message: "Falta el queso de la pizza", preferredStyle: .Alert)
+            let cancela = UIAlertAction(title: "Reintentar", style: .Cancel, handler: nil)
+            alerta.addAction(cancela)
+            self.presentViewController(alerta, animated:true, completion:nil)
+            case ingrediente:
+            print("no hay ingrediente")
+            let alerta = UIAlertController(title: "Atencion!", message: "Faltan los ingredientes de la pizza", preferredStyle: .Alert)
+            let cancela = UIAlertAction(title: "Reintentar", style: .Cancel, handler: nil)
+            alerta.addAction(cancela)
+            self.presentViewController(alerta, animated:true, completion:nil)
+            default:
+            print("Hay todo")
+            let alerta = UIAlertController(title: "Confirmado !", message: "Su pizza esta completa !", preferredStyle: .Alert)
+            let confirma = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alerta.addAction(confirma)
+            self.presentViewController(alerta, animated:true, completion:nil)
+        }
+    }
 }
 
